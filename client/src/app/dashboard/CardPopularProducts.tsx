f0176bee4bee6ca65030eaa640a9db5a20a7a3cd
Utsav Bajracharya <utsavbajra619@gmail.com>
2026-03-27 00:11:39 -0400

    product layout divider

--- client/src/app/dashboard/CardPopularProducts.tsx
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -12,9 +12,32 @@ const CardPopularProducts = () => {
                 <h3 className="text-lg font-semibold px-7 pt-5 pb-2">
                     Popular Products
                 </h3>
-                </>
-            )}
-        </div>
+                <hr />
+                <div className="overflow-auto h-full">
+                    {dashboardMetrics?.popularProducts.map((product) => (
+                        <div
+                            key={product.productId}
+                            className="flex items-center justify-between gap-3 px-5 py-7 border-b"
+                        >
+                                <div className="flex items-center gap-3">
+                                    <div className="flex flex-col justify-between gap-1">
+                                        <div className="font-bold text-gray-700">
+                                            {product.name}
+                                        </div>
+                                        <div className="flex text-sm items-center">
+                                            <span className="font-bold text-blue-500 text-xs">
+                                                ${product.price}
+                                            </span>
+                                        <span className="mx-2">|</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </>
+        )}
+    </div>
     );
 };
 
