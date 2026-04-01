7485c7887468cefc4f790ade871bbe553dd3ba4f
Utsav Bajracharya <utsavbajra619@gmail.com>
2026-04-01 00:04:08 -0400

    add inventory function

--- client/src/app/inventory/page.tsx
+++ client/src/app/inventory/page.tsx
@@ -28,4 +28,33 @@ const columns: GridColDef[] = [
     },
 ];
 
+const Inventory = () => {
+    const { data: products, isLoading, isError } = useGetProductsQuery();
 
+
+    if (isLoading) {
+        return <div className="py-4">Loading...</div>;
+    }
+
+    if (isError || !products) {
+        return (
+            <div className="text-center text-red-500 py-4">
+                Failed to fetch products. Please try again later.
+             </div>
+        );
+    }   
+        
+    return (
+        <div className="p-4">
+            <DataGrid
+                rows={products}
+                columns={columns}
+                getRowId={(row) => row.productId}
+                checkboxSelection
+                className="bg-white shadow rounded-lg border border-gray-200 mt-5 !text-gray-700"
+            />
+        </div>
+    );
+};
+
+export default Inventory;
