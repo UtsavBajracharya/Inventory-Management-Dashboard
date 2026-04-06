d94095565d86a5613d46458502021370260172a2
Utsav Bajracharya <utsavbajra619@gmail.com>
2026-04-05 23:08:19 -0400

    add expense controller and routes

---
+++ server/src/routes/expenseRoutes.d.ts
@@ -0,0 +1,3 @@
+import type { Request, Response } from "express";
+export declare const getExpensesByCategory: (req: Request, res: Response) => Promise<void>;
+//# sourceMappingURL=expenseRoutes.d.ts.map
