dba201483fecce88dcfa7e6cf90e1b7c3e5bab54
Utsav Bajracharya <utsavbajra619@gmail.com>
2026-04-05 23:13:47 -0400

    import routes in index

--- server/src/routes/expenseRoutes.ts
+++ server/src/routes/expenseRoutes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { getExpensesByCategory } from "../controllers/expenseController.js";
+
 const router = Router();
 
 router.get("/", getExpensesByCategory);
